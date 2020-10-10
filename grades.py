cd4fcc4c405fe55d937602a9a05cee25b697b4fd
jonnycross10 <crossjonny5@gmail.com>
2020-10-10 00:24:11 -0700

    grades can print dict of course with grades
    It contains a bunch of extra info, so I will change it to only have the course name and grade, and for all courses

--- grades.py
+++ grades.py
@@ -1,10 +1,14 @@
 from canvasapi import Canvas
+from secrets import apiKey
 
 canvas = Canvas(url,apiKey)
 
 usr = canvas.get_current_user()
 
-l = usr.get_courses(include = "total_scores")
+l = usr.get_courses()
 
-#e = usr.get_enrollments(include[ "current_points"])
-print(canvas.get_department_level_grade_data_current)
+headers = {"Authorization":"Bearer "+apiKey+"", "Content-Type": "json"
+ }
+
+r = requests.get("https://csus.instructure.com/api/v1/courses/64923/users/99221?include[]=enrollments", headers = headers)
+print(r.text)
